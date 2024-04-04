f2c3467c9af94ad1dbeb055ef556de328c4429ac
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-04-04 16:02:54 +0400

    commit

--- src/app/(pages)/product/[id]/page.tsx
+++ src/app/(pages)/product/[id]/page.tsx
@@ -2,7 +2,6 @@
 import { DataType, GlobalContext } from '@/app/Data';
 import BestGear from '@/app/components/BestGear';
 import Category from '@/app/components/Category';
-import { useSearchParams } from 'next/navigation';
 import { useContext, useState } from 'react';
 
 type ParamsType = {
@@ -12,13 +11,12 @@ type ParamsType = {
 };
 
 export default function page({ params }: ParamsType) {
+  const [obj, setObj] = useState();
   const context = useContext(GlobalContext);
   if (!context) return null;
   const { data } = context;
   const newData = data.filter((el) => el.id === Number(params.id));
-  const [obj, setObj] = useState(newData[0]);
 
-  console.log(obj);
   return (
     <div className="flex flex-col px-4 items-center justify-center">
       {newData[0].name}
