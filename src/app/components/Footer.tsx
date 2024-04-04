3819ff47c5fdadc96614ec92d805e8added38974
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-04-04 16:14:49 +0400

    commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -35,10 +35,10 @@ export default function Footer() {
         </Link>
       </div>
       <p className="opacity-50 text-white text-[15px]">
-        Audiophile is an all in one stop to fulfill your audio needs. We're a small team
-        of music lovers and sound specialists who are devoted to helping you get the most
-        out of personal audio. Come and visit our demo facility - we&apos;re open 7 days a
-        week.
+        Audiophile is an all in one stop to fulfill your audio needs. We&apos;re a small
+        team of music lovers and sound specialists who are devoted to helping you get the
+        most out of personal audio. Come and visit our demo facility - we&apos;re open 7
+        days a week.
       </p>
       <p className="font-bold text-white text-[15px] opacity-50">
         Copyright 2021. All Rights Reserved
